1b156496e80f5c52015e00a55498dbee11cda238
ETF-Student1 <etf1@msil.com>
2019-12-16 12:40:58 +0100

    djeljenje

--- Lab03projekat/main.c
+++ Lab03projekat/main.c
@@ -5,9 +5,15 @@ int korijen(int n)
 {
 	printf("Korijen broja %d je %d.",n,sqrt(n));
 }
+
 int zbir(int a, int b)
 {
     return a+b;
+
+int djeljenje(int a, int b)
+{
+    return a/b;
+
 }
 int main()
 {
