6c377c882237e5e03eee0b81a445fbb65964079f
PC02\Student <student@etfbl.net>
2023-11-13 14:41:12 +0100

    main

--- main.c
+++ main.c
@@ -1,4 +1,27 @@
+#include <math.h>
+#include <>
+
 int sabiranje2broja(int a, int b)
 {
     return a + b;
+}
+
+int main()
+{
+    int a=4,b=7,c=2,d=1,e=5,f=6,g=3;
+    int najmanji_od_n_brojeva=0;
+    int najveci_od_n_brojeva=0;
+    int srednja_vrijednost=0;
+    int stepenovanje_broja=0;
+    int kvadratni_korjen=0;
+    int dijeljenje_2_broja=0;
+    int mnozenje_n_brojeva=0;
+    int mnozenje_2_broja=0;
+    int oduzimanje_n_brojeva=0;
+    int oduzimanje_2_broja=0;
+    int sabiranje_n_brojeva=0;
+    int sabiranje_2_broja=0;
+
+
+    return 0;
 }
