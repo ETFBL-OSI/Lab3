7de0976bccf994f7cdb64f4dc06c4c5929911a60
PC18\Student <student@etfbl.net>
2023-11-13 14:49:03 +0100

    mnozenje n brojeva

---
+++ main.c
@@ -0,0 +1,13 @@
+
+
+
+int proizvod_n_brojeva(int n)
+{
+    printf("Unesite %d brojeva:\n", n);
+    for (i = 0; i < n; ++i) {
+    int broj;
+    scanf("%d", &broj);
+    proizvod *= broj;
+
+    return proizvod;
+}
