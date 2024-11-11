2858671ea64e4efbeba95cc2b91445e3f7204726
PC16\Student <student@etfbl.net>
2024-11-11 14:42:37 +0100

    srednja vrijednost

--- Lab3-2/main.c
+++ Lab3-2/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float srednja_vr(int a, int b) {
+
+    return (a+b)/2;
+}
+
 int main()
 {
+    float c;
+
+    c = srednja_vr(2, 5);
 
     return 0;
 }
