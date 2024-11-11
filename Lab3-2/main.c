d22b92e53760ade982cd1fbe0461e5aa54a14162
PC02\Student <student@etfbl.net>
2024-11-11 14:46:28 +0100

    mnozenje n brojeva

--- Lab3-2/main.c
+++ Lab3-2/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mnozenje_n(int n, int niz[])
+{
+    int result=1;
+    for (int i=0; i<n;i++)
+    {
+        result*=niz[i];
+    }
+return result;
+}
+
 int main()
 {
+
+    int arr[3]={1,2,3};
+    int m=mnozenje_n(3,&arr);
+   printf("%d", m);
 
     return 0;
 }
