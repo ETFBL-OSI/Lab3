509332d638095156c6a79fe7e01ceaca6b81d3a7
PC17\Student <student@etf.bl.net>
2024-11-11 14:42:39 +0100

    Stepenovanje broja

--- Lab3-2/main.c
+++ Lab3-2/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int StepenovanjeBroja(int Broj, int Stepen);
+
 int main()
 {
-
+    printf("%d", StepenovanjeBroja(2, 3));
     return 0;
+}
+
+int StepenovanjeBroja(int Broj, int Stepen)
+{
+    int rez = 1;
+    for (int i = Stepen; i < 0; i--)
+        rez *= Broj;
+    return rez;
 }
