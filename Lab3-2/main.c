a9907c07b5134f0030e99efd9a42a549fa824809
PC07\Student <student@etfbl.net>
2024-11-11 14:39:58 +0100

    Funkcija za mnozenje dva broja

--- Lab3-2/main.c
+++ Lab3-2/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mnozenje(int a, int b){
+return a*b;
+}
+
 int main()
 {
+    int a,b;
+    printf ("Unesite broj a: ");
+    scanf ("%d" , &a);
+    printf ("Unesite broj b: ");
+    scanf ("%d" , &b);
+    int rezultat = mnozenje(a,b);
+    printf ("rezultat je %d", rezultat);
 
-    return 0;
+  return 0;
 }
